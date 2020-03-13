ba222b0a942acc018a6ca5e15aa97f4f6f05bf2f
Amit Prabhu <30145484+asp48@users.noreply.github.com>
2020-03-13 21:53:25 +0530

    Update Shortest_Unsorted_Continuous_Subarray.java

--- Arrays/Shortest_Unsorted_Continuous_Subarray.java
+++ Arrays/Shortest_Unsorted_Continuous_Subarray.java
@@ -12,6 +12,56 @@ Then length of the input array is in range [1, 10,000].
 The input array may contain duplicates, so ascending order here means <=.
 */
 
+/*
+Intuition
+An unsorted array is basically elements at the wrong positions compared to what their positions would have been in a sorted version of the same array. If we know the correct position of each element in the sorted version of the complete array, then the leftmost element which is at the incorrect position marks the starting index of required subarray. Similarly the righmost element which is at the incorrect position marks the end of required subarray.
+
+Approach 1 :  Brute Force
+Take each possible subarray (i,j) and check if (0,i-1) is sorted and less than min of (i,j), similarly (j,n-1) is sorted and greater than max(i,j)
+Time Complexity: O(n^3), (n^2) for generating subarrays and (n) for checking conditions for each subarray
+Space Complexity: O(1)
+
+Approach 2 : Optmized Brute Force
+for each i from 0 to n-1
+    for each j from i+1 to n-1
+        if array[i] > array[j] // implies elements are in wrong positions
+            find min incorrect position
+            find max incorrect position
+ans = max incorrect - min incorrect + 1 // +1 because boundary elements are inclusive
+Time Complexity: O(n^2)
+Space Complexity; O(1)
+
+Approach 3 : Using sorting
+Compare given array with sorted version of the same array
+ans = last mismatch - first mismatch
+Time Complexity: O(nlogn)
+Space Complexity: O(n), copy of original is used for sorting
+
+Approach 4: Using Stack
+for each i from 0 to n-1
+    if array[i] > stack.peek()
+        stack.push(i)  // follows ascending order
+    else 
+        while stack is not empty && array[i] < stack.peek()
+            k = stack.pop()
+            start = min(k, start) // find minimum such k
+        stack.push(i)  // put the current index in its right position
+Use the same above logic to find end
+ans = end - start + 1 
+Time Complexity: O(2*n) looped two times to find start and end
+Space Complexity: O(n) Stack size may grow at max n
+
+Approach 5: Optimized version of stack solution, with no extra space
+for i from 1 to n-1
+    if array[i] < array[i-1]   // wrong order, incorrect position
+        min = find min(array, i, n-1) // find min starting from i to end of array
+        start = find correct position of min within (0, i) 
+        // array to the left of i is already sorted, so the correct position is k-1 where k is the index of first element > min
+Use the same above logic to find end
+ans = end-start+1
+Time Complexity: O(4*n), 2*(n for finding min/max + n for finding correct position of min/max)
+Space Complexity: O(1)
+*/
 class Solution {
     public int findUnsortedSubarray(int[] nums) {
         int start=0, end=0, n=nums.length, min=Integer.MAX_VALUE, max=Integer.MIN_VALUE;
