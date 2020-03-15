5b907a2740bb6266ff66b61afc1ccf169bc291cc
Amit Prabhu <30145484+asp48@users.noreply.github.com>
2020-03-16 01:30:46 +0530

    Update Permutations.java

--- Bfs, Dfs/Permutations.java
+++ Bfs, Dfs/Permutations.java
@@ -58,3 +58,51 @@ class Solution {
         return result;
     }
 }
+
+/*
+Using backtracking.
+Recursion call looks as below.
+      ------------------  
+     /        |          \
+    /         |           \
+   1          2            3
+  / \        / \          / \
+ 2   3      1   3        1   2
+ |   |      |   |        |   |
+ 3   2      3   1        2   1
+ When we reach the leaf node i.e CurrentPermutationList.size()==nums.length, we add the list to final result.
+ Each path in the above recursion tree contributes to a unique permutation list
+*/
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        helper(nums, result, new ArrayList<>(), new boolean[nums.length]);
+        return result;
+    }
+    
+    private void helper(int[] nums, List<List<Integer>> result, List<Integer> curList, boolean[] used){
+        // Base Condition: Reached leaf node
+        if(curList.size() == nums.length){
+            // add current list to final result
+            result.add(new ArrayList<>(curList));
+            return;
+        }
+        // always start from index 0
+        for(int i=0;i<nums.length;i++){
+            // skip if element is already used
+            if(used[i]){
+                continue;
+            }
+            // add the element to current list
+            curList.add(nums[i]);
+            // set the current element as used
+            used[i] = true;
+            // generate all permutations possible with unused elements
+            helper(nums, result, curList, used);
+            // Remove the current element, to backtrack
+            curList.remove(curList.size()-1);
+            // set the current element as unused
+            used[i] = false;
+        }
+    }
+}
